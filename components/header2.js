41ab04a19699d317da8f9050efa411ff5319c91c
Marceljanes <marceljanes@gmx.de>
2022-06-12 19:45:17 +0200

    all

--- components/header2.js
+++ components/header2.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 export default function Header2({jobs, setJobs}) {
     
-    const dev_url = process.env.DEV_URL   
+     
 
     return (
         <div className="flex justify-center items-center w-full md:mt-5 md:mb-5">
