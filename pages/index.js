6a381ee61a3e45dcdbf27476f7a21150096dd529
Marceljanes <marceljanes@gmx.de>
2022-06-12 19:54:29 +0200

    all

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library  
   //const res = await fetch(`https://newpolitikjobs.vercel.app/api/hello`)
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/hello`)
+  const res = await fetch(`https://newpolitikjobs.vercel.app//api/hello`)
   // const res = await fetch('http://localhost:3000/api/hello')  
   const posts = await res.json()
  
